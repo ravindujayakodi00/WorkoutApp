ea32ac89bc5cb2b6ab0aa33a6a9046ae5e4fb658
Ravindu Jayakodi <109682931+ravindujayakodi00@users.noreply.github.com>
2022-12-23 21:46:50 +0530

    Auth context

---
+++ frontend/src/hooks/useAuthContext.js
@@ -0,0 +1,14 @@
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+
+export const useWorkoutsContext = () => {
+    const context = useContext(AuthContext);
+
+    if (!context) {
+        throw Error(
+            "useAuthContext must be used within AuthContextProvider"
+        );
+    }
+
+    return context;
+}
