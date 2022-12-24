a5478a8150870d5f0973c5e591bde874afa175e1
Ravindu Jayakodi <109682931+ravindujayakodi00@users.noreply.github.com>
2022-12-25 01:02:29 +0530

    useLogin

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { useAuthContext } from './useAuthContext';
+
+export const useLogin = () => {
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+    const [success, setSuccess] = useState(null)
+    const {dispatch} = useAuthContext()
+  
+    const login = async (email,password) => {
+        setIsLoading(true)
+        setError(null)
+
+        const response = await fetch('/api/user/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({email,password})
+        })
+
+        const json = await response.json()
+
+        if (!response.ok) {
+            setIsLoading(false)
+            setError(json.message)
+        }
+        if (response.ok) {
+            //save the user to local storage
+            localStorage.setItem('user', JSON.stringify(json)) 
+
+            //update the context
+            dispatch({type: 'LOGIN', payload: json})
+
+            setIsLoading(false)
+
+            setSuccess('You have successfully signed up!')
+
+
+        } 
+    }
+    
+    return {login, error, isLoading, success}
+}
